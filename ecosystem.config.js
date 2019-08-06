0455f9b457c552258b589bf5bf4db8b2c78752fa
Josh Goodman <jogoodma@iu.edu>
2019-08-06 16:13:05 -0400

    Added README and reworked config.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'alleles',
+      name: 'flybase-graphql',
       script: 'dist/bundle.js',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
       args: '',
@@ -9,26 +9,26 @@ module.exports = {
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      output: './logs/graphql-server.log',
-      error: './logs/graphql-server.err',
+      output: './logs/flybase-graphql.log',
+      error: './logs/flybase-graphql.err',
       env: {
-        NODE_ENV: 'development'
+        NODE_ENV: 'development',
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
       }
     },
     {
       name: 'postgraphile',
       script: './node_modules/.bin/postgraphile',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      args: '-c postgres:///FB2019_02 -s flybase,gene -a -j -M',
+      args: '-s flybase,gene -a -j -M',
       instances: 1,
       autorestart: true,
       watch: false,
       max_memory_restart: '1G',
-      output: './logs/chado-server.log',
-      error: './logs/chado-server.err',
+      output: './logs/postgraphile.log',
+      error: './logs/postgraphile.err',
       env: {
         NODE_ENV: 'development',
       },
