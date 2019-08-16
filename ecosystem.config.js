31b74e243517bd25d4da85a08a715a2fba00745e
Josh Goodman <jogoodma@iu.edu>
2019-08-16 16:00:55 -0400

    Upping HTTP payload size.

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -23,7 +23,6 @@ module.exports = {
       name: 'postgraphile',
       script: './node_modules/.bin/postgraphile',
       // Options reference: https://pm2.io/doc/en/runtime/reference/ecosystem-file/
-      args: '-s flybase,gene -a -j -M',
       instances: 1,
       autorestart: true,
       watch: false,
@@ -32,9 +31,11 @@ module.exports = {
       error: './logs/postgraphile.err',
       env: {
         NODE_ENV: 'development',
+        args: '-s flybase,gene -j -M -l 5MB --timeout 60000',
       },
       env_production: {
-        NODE_ENV: 'production'
+        NODE_ENV: 'production',
+        args: '-s flybase,gene -j -M -l 5MB --timeout 60000 --disable-query-log',
       }
     }
   ],
