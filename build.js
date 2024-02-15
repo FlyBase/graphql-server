a4ca4f33ec1aec4e0d3645f2dd9068ff627457df
Seth Campbell <sercampb@iu.edu>
2024-02-15 19:21:14 +0000

    Adds transgenic product class list to scheme

--- build.js
+++ build.js
@@ -1,14 +1,14 @@
 const esbuild = require('esbuild')
 const loader = require('@luckycatfactory/esbuild-graphql-loader').default
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+//const yargs = require('yargs/yargs')
+//const { hideBin } = require('yargs/helpers')
+//const argv = yargs(hideBin(process.argv)).argv
 
 esbuild
   .build({
     entryPoints: ['server/index.js'],
     platform: 'node',
-    watch: argv.watch,
+    watch: false/* argv.watch */,
     bundle: true,
     target: 'node10.24.0',
     outfile: 'dist/bundle.js',
